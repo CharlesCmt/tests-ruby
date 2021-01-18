d24c0154d11b54d4c6289cb6b3bf341437bc1611
Charles <charlescampet03@gmail.com>
2021-01-18 19:39:05 +0100

    update

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,35 +1,32 @@
 #Translation
-=begin
-def translate(string)
-    vowel = ("aeiou")
-    array = string.split(" ").map{|word| word.include?(vowel) ? word}.join(" ")
-    array += "ay"
-end
-=end
-
 def translate(string)
     array = string.split(" ")
-    pigged_array = array.map! {|x| pigify(x)}
-    result = pigged_array.join(" ")
+    cut_array = array.map! {|x| cut(x)}
+    result = cut_array.join(" ")
     return result
 end
 
-def pigify(word)
+def cut(word)
     vowels = ["a", "e", "i", "o", "u"]
   
-    if vowels.include? word[0].downcase
+    case word
+    when vowels.include? word[0].downcase
       word + "ay"
+
     # two cases for "qu"
-    elsif word[0..1] == "qu"
+    when word[0..1] == "qu"
       word[2..-1] + "quay"
-    elsif word[1..2] == "qu"
+    when word[1..2] == "qu"
       word[3..-1] + word[0..2] + "ay"
+
     # for words that start with 3 consonants
-    elsif !(vowels.include? word[0]) && !(vowels.include? word[1]) && !(vowels.include? word[2])
+    when !(vowels.include? word[0]) && !(vowels.include? word[1]) && !(vowels.include? word[2])
       word[3..-1] + word[0..2] + "ay"
+
     # for words that start with 2 consonants
-    elsif !(vowels.include? word[0]) && !(vowels.include? word[1]) # for 2
+    when !(vowels.include? word[0]) && !(vowels.include? word[1]) # for 2
       word[2..-1] + word[0..1] + "ay"
+
     # for words that start with a single consonant
     else
       word[1..-1] + word[0] + "ay"
